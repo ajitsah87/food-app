cc3ff2f57aa992554e5d64e70bfae89d2799cfdc
vishal <121763346+vishal-gg@users.noreply.github.com>
2023-09-20 17:55:44 +0530

    added smooth scroll

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,6 +1,26 @@
+import { useMotionValueEvent, useScroll } from "framer-motion";
+import { useRef, useState } from "react";
+
 const Home = () => {
+  const [value, setValue] = useState(0);
+
+  const containerRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: containerRef,
+    offset: ["start start", "end start"],
+  });
+
+  useMotionValueEvent(scrollYProgress, "change", (value) =>
+    setValue(Number(value * 100))
+  );
+
   return (
-    <div id="/home" className="bg-blend-multiply bg-black/50 bg-landingPage h-screen grid place-content-center bg-cover landing_page">
+    <div
+      style={{ backgroundPositionY: -value }}
+      ref={containerRef}
+      id="/home"
+      className="bg-blend-multiply bg-black/50 bg-landingPage h-screen grid place-content-center bg-cover landing_page"
+    >
       <div className="text-white text-7xl font-bold">
         <h1>home</h1>
       </div>
